2b467a680ea8139787600a553928e4b1b846cb56
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-07-17 17:31:28 +0530

    footer changed

--- resources/views/Frontend/Partials/footer/footer-v3.blade.php
+++ resources/views/Frontend/Partials/footer/footer-v3.blade.php
@@ -1,18 +1,19 @@
 <!-- footer start -->
-<footer class="rts-footer {{ $class ?? ''}}">
+<footer class="rts-footer {{ $class ?? '' }}">
     <div class="container">
         <div class="row gy-5 gy-lg-0">
             <div class="col-lg-4 col-md-6 col-sm-6">
                 <div class="rts-footer-widget w-320">
                     <a href="{{ route('index') }}" class="d-block rts-footer-logo mb--40">
-                        <img src="{{asset('assets/images/logo/logo__white.svg')}}" alt="Unipix">
+                        <img src="{{ asset('assets/images/logo/logo__white.svg') }}" alt="Unipix">
                     </a>
                     <p>
-                        We are passionate education dedicated to providing high-quality 
+                        We are passionate education dedicated to providing high-quality
                         resources learners all backgrounds.
                     </p>
                     <div class="rts-contact-link">
-                        <a href="mailto:contact@reacthemes.com"><i class="fa-sharp fa-light fa-location-dot"></i> Park, Melbourne, Australia </a>
+                        <a href="mailto:contact@reacthemes.com"><i class="fa-sharp fa-light fa-location-dot"></i> Park,
+                            Melbourne, Australia </a>
                         <a href="callto:121"><i class="fa-thin fa-phone"></i> 485-826-710</a>
                     </div>
                 </div>
@@ -34,15 +35,15 @@
             <div class="col-lg-2 col-md-6 col-sm-4">
                 <div class="rts-footer-widget ml--30">
                     <h6 class="rt-semi">Our Campus</h6>
-                        <div class="rts-footer-menu">
-                            <ul>
-                                <li><a href="{{ route('about') }}">About </a></li>
-                                <li><a href="{{ route('tution-fee') }}">Tution Fee</a></li>
-                                <li><a href="{{ route('alumni') }}">Alumni</a></li>
-                                <li><a href="{{ route('faculty') }}">Faculty Staff</a></li>
-                                <li><a href="{{ route('event') }}">Event</a></li>
-                            </ul>
-                        </div>
+                    <div class="rts-footer-menu">
+                        <ul>
+                            <li><a href="{{ route('about') }}">About </a></li>
+                            <li><a href="{{ route('tution-fee') }}">Tution Fee</a></li>
+                            <li><a href="{{ route('alumni') }}">Alumni</a></li>
+                            <li><a href="{{ route('faculty') }}">Faculty Staff</a></li>
+                            <li><a href="{{ route('event') }}">Event</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 col-sm-8">
@@ -52,7 +53,7 @@
                         <ul class="list-unstyled">
                             <li class="single-post">
                                 <a href="{{ route('blog-details') }}" class="blog-thumb">
-                                    <img src="{{asset('assets/images/blog/w-1.jpg')}}" alt="latest post">
+                                    <img src="{{ asset('assets/images/blog/w-1.jpg') }}" alt="latest post">
                                 </a>
                                 <div class="post-content">
                                     <span class="rt-date">October 29, 2023</span>
@@ -63,7 +64,7 @@
                             </li>
                             <li class="single-post">
                                 <a href="{{ route('blog-details') }}" class="blog-thumb">
-                                    <img src="{{asset('assets/images/blog/small-thumb-1.jpg')}}" alt="latest post">
+                                    <img src="{{ asset('assets/images/blog/small-thumb-1.jpg') }}" alt="latest post">
                                 </a>
                                 <div class="post-content">
                                     <span class="rt-date">October 29, 2023</span>
@@ -83,7 +84,8 @@
     <div class="container">
         <div class="row">
             <div class="rt-center">
-                <p class="--p-xs">Copyright &copy; {{date ('Y')}} All Rights Reserved by <a href="#">Unipix</a></p>
+                <p class="--p-xs">Copyright &copy; {{ date('Y') }} Pioneer Convent Designed & Developed by <a target="_blank"
+                        href="https://www.bridgekala.com/">BridgeKala</a></p>
             </div>
         </div>
     </div>
