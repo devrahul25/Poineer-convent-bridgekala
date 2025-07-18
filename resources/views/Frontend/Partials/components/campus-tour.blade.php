8810bd2c1bf1c8e8308b04f7942524bc53500eef
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-07-18 15:29:50 +0530

    about page created

--- resources/views/Frontend/Partials/components/campus-tour.blade.php
+++ resources/views/Frontend/Partials/components/campus-tour.blade.php
@@ -4,16 +4,24 @@
             <h2 class="section-title rt-center mb--50">Our Campus Tour</h2>
             <div class="col-12">
                 <div class="rts-video-section height-500 mb--50">
-                    <a href="https://www.youtube.com/watch?v=Uwq1uiuM_9w" class="rts-video-section-player popup-video video-btn">
+                    <a href="https://www.youtube.com/watch?v=3ItqSTGHtzc"
+                        class="rts-video-section-player popup-video video-btn">
                         <i class="fa-sharp fa-solid fa-play"></i>
                     </a>
-                    <img src="{{asset('assets/images/about/video-thumb.jpg')}}" alt="video-bg">
+                    <img src="{{ asset('assets/images/about/video-thumb.jpg') }}" alt="video-bg">
                 </div>
             </div>
             <div class="col-lg-8">
                 <div class="rts-video-section-text rt-center mx-3">
-                    <p>Embark on a journey of knowledge, discovery, and growth at  Unipix University. Our admissions process is designed to identify bright, motivated individuals who are eager to contribute to our dynamic academic community. Whether you're a recent high school graduate or a transfer student seeking a new academic home, we invite you to explore the possibilities that await you.</p>
-                    <a href="{{ route('campus-life') }}" class="mt--15 about-btn rts-nbg-btn btn-arrow">Visit Campus <span><i class="fa-sharp fa-regular fa-arrow-right"></i></span></a>
+                    <p>Step into the vibrant world of Pioneer Convent Sr. Sec. School—a 2-acre lush green campus
+                        designed for academic excellence, creativity, and holistic growth. From state-of-the-art science
+                        and computer labs to open sports grounds and interactive learning zones, every corner of our
+                        school nurtures knowledge and character. Whether you’re exploring classrooms, activity hubs, or
+                        co-curricular clubs, you’ll find an inspiring environment where students thrive. Join us for a
+                        glimpse into a space where values meet vision and every child discovers their true potential.
+                    </p>
+                    <a href="{{ route('campus-life') }}" class="mt--15 about-btn rts-nbg-btn btn-arrow">Visit Campus
+                        <span><i class="fa-sharp fa-regular fa-arrow-right"></i></span></a>
                 </div>
             </div>
         </div>
