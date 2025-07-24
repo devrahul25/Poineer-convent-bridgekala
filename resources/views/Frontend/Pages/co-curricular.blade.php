1ca080db7addd5137851c6e7d5bd26ae23f817f1
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-07-24 14:56:10 +0530

    created cocurricular page

--- resources/views/Frontend/Pages/co-curricular.blade.php
+++ resources/views/Frontend/Pages/co-curricular.blade.php
@@ -4,7 +4,7 @@
     @include($elements . 'breadcrumb', [
         'class' => 'breadcrumb-height breadcumb-bg',
         'image' => 'breadcrumb.jpg',
-        'title' => 'About Pioneer Convent',
+        'title' => 'Co-Curricular and Cultural Activities at Pioneer',
         'page' => 'about',
     ])
 
@@ -12,86 +12,79 @@
     <section class="rts-about-university rts-section-padding">
         <div class="container">
             <div class="row">
-                <div class="rts-section">
-                    <div class="col-lg-4 col-md-5">
-                        <h3 class="rts-section-title">About School</h3>
-                    </div>
-                    <div class="col-lg-8 col-md-7">
-                        <p class="rts-section-description">
-                            Established in 1995, Pioneer Convent Sr. Sec. School has been shaping young minds through
-                            quality education, strong values, and holistic development, creating responsible and confident
-                            individuals prepared to face future challenges with knowledge, integrity, and compassion.
+
+                <div
+                    class="col-lg-12 col-md-12 text-center d-flex flex-column justify-content-center align-items-center mb-5">
+                    <h5 style="font-size: 32px;"> "Education without co-curricular activities is like educating the mind
+                        without educating the soul."
+                    </h5>
+                    <div class="col-lg-11">
+                        <p>
+                            At Pioneer, we firmly believe that education goes beyond textbooks. Co-curricular activities
+                            such as
+                            debates, declamations, singing, dancing, sports, recitation, short plays, quizzes, and many more
+                            are
+                            an integral part of our school curriculum. These activities offer every child a platform to
+                            express
+                            themselves, build confidence, and foster a spirit of independence.
+                            Through this diverse range of experiences, Pioneerians learn the value of teamwork, leadership,
+                            responsibility, and time management. Co-curricular involvement also nurtures moral and social
+                            values, playing a pivotal role in honing innate talents and shaping our students into sensitive,
+                            responsible global citizens.
                         </p>
                     </div>
                 </div>
             </div>
-            <div class="row g-5 justify-content-md-center justify-content-start">
-                <div class="col-lg-7 col-xl-8 col-md-11">
-                    <div class="rts-about-section">
-                        <img src="{{ asset('assets/images/about/about-01.jpg') }}" alt="">
-                    </div>
-                </div>
-                <div class="col-lg-5 col-xl-4 col-md-11">
-                    <div class="rts-about-details">
-                        <div class="single-about-info">
-                            <div class="content">
-                                <h3 class="title">1,800+</h3>
-                                <img src="{{ asset('assets/images/icon/11.svg') }}" alt="">
-                            </div>
-                            <div class="desc">
-                                <p>Enrolled Students from Nursery to Class 12
-                                </p>
-                            </div>
-                        </div>
-                        <div class="single-about-info">
-                            <div class="content">
-                                <h3 class="title"> 120+</h3>
-                                <img src="{{ asset('assets/images/icon/12.svg') }}" alt="">
-                            </div>
-                            <div class="desc">
-                                <p>Experienced Faculty & Staff</p>
-                            </div>
-                        </div>
-                        <div class="single-about-info">
-                            <div class="content">
-                                <h3 class="title">3,500+</h3>
-                                <img src="{{ asset('assets/images/icon/13.svg') }}" alt="">
-                            </div>
-                            <div class="desc">
-                                <p>Pioneer Alumni Across India</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        </div>
         </div>
     </section>
     <!-- about university end -->
 
     <!-- history -->
-    <div class="rts-history">
+    <div class="rts-history" style="margin-bottom: 100px">
         <div class="container">
             <div class="row g-5 justify-content-md-center justify-content-start align-items-center">
                 <div class="col-lg-6 col-md-11">
                     <div class="rts-history-image">
-                        <img src="{{ asset('assets/images/about/history.jpg') }}" alt="history">
+                        <img src="{{ asset('assets/images/Co-curricular/cult.jpg') }}" alt="history">
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-11">
                     <div class="rts-history-section">
-                        <h4 class="rts-section-title mb--40">Our Story</h4>
+                        <h4 class="rts-section-title mb--40">Cultural Activities</h4>
                         <p>
-                            Founded in 1995, Pioneer Convent Sr. Sec. School has grown into one of Delhi’s most respected
-                            CBSE-affiliated institutions. Nestled in the heart of Loknayakpuram, our 2-acre lush green
-                            campus provides a serene and stimulating environment where students can learn, grow, and thrive.
-                            From pre-primary to Class 12, we offer all academic streams, supported by modern infrastructure
-                            and a team of passionate educators.
-                            <span class="d-block mb--30"></span>
-                            At Pioneer, we follow a child-centric approach inspired by the globally recognized Multiple
-                            Intelligence Theory. Our aim is to recognize and nurture each student’s unique potential through
-                            personalized learning experiences. By integrating academic excellence with co-curricular
-                            activities, life skills, and moral values, we empower our students with the knowledge,
-                            confidence, and integrity needed to lead meaningful and successful lives.
+                            Cultural programs are key to holistic development. At Pioneer, we believe that participation in
+                            cultural activities encourages students to explore their creative sides while gaining life
+                            skills and social awareness. These activities instill confidence, deepen understanding of one’s
+                            cultural roots, and prepare students to meet the challenges of the 21st century with clarity and
+                            resilience.
+
+                        </p>
+                        <p>We offer a rich array of cultural experiences through:
+                            <li><b>Special Assemblies</b> to celebrate national and local festivals</li>
+                            <li><b>Art & Craft Exhibitions</b></li>
+                            <li><b>Dance and Drama Performances</b></li>
+                            <li><b>Inter-House Cultural Competitions</b></li>
+                        </p>
+                        <p>These platforms give students the opportunity to perform, collaborate, and grow in a dynamic
+                            learning environment.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- history end-->
+    <!-- history -->
+    <div class="rts-history" style="margin-bottom: 100px">
+        <div class="container">
+            <div class="row g-5  justify-content-center align-items-center" style="text-align: center">
+
+                <div class="col-lg-6 col-md-11">
+                    <div class="rts-history-section">
+                        <h4 class="rts-section-title mb--40">Student Clubs at Pioneer</h4>
+                        <p>
+                            To further enrich student life, we offer a variety of vibrant clubs that help students explore
+                            their passions and discover new interests:
                         </p>
                     </div>
                 </div>
@@ -99,89 +92,91 @@
         </div>
     </div>
     <!-- history end-->
-
-    <!-- funfact -->
-    <div class="rts-funfact rts-section-padding">
+    <!-- history -->
+    <div class="rts-history" style="margin-bottom: 100px">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-10 ">
-                    <div class="rts-funfact-wrapper">
-                        <div class="single-cta-item">
-                            <h2 class="single-cta-item__title">100%</h2>
-                            <p>Board Exam Success Rate</p>
-                        </div>
-                        <div class="single-cta-item">
-                            <h2 class="single-cta-item__title">Top Ranked</h2>
-                            <p>Among Child-Centric Schools</p>
-                        </div>
-                        <div class="single-cta-item">
-                            <h2 class="single-cta-item__title">Since 1995</h2>
-                            <p>Trusted Legacy of Education</p>
-                        </div>
+            <div class="row g-5 justify-content-md-center justify-content-start align-items-center">
+
+                <div class="col-lg-6 col-md-11">
+                    <div class="rts-history-section">
+                        <h4 class="rts-section-title mb--40">Dramatic Club</h4>
+                        <p>
+                            The Dramatics Club encourages students to express ideas and emotions through performance. It
+                            builds confidence, enhances oratory skills, and fosters empathy through role-play and
+                            storytelling.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-11">
+                    <div class="rts-history-image">
+                        <img src="{{ asset('assets/images/Co-curricular/drama.jpg') }}" alt="history">
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- funfact end -->
-
-
-    <!-- mission -->
-    {{-- <section class="rts-mission">
+    <!-- history end-->
+    <!-- history -->
+    <div class="rts-history" style="margin-bottom: 100px">
         <div class="container">
-            <div class="row justify-content-center rt-center">
-                <div class="rts-section mb--50">
-                    <h2 class="rts-section-title">Mission and Vision</h2>
+            <div class="row g-5 justify-content-md-center justify-content-start align-items-center">
+                <div class="col-lg-6 col-md-11">
+                    <div class="rts-history-image">
+                        <img src="{{ asset('assets/images/Co-curricular/art.png') }}" alt="history">
+                    </div>
                 </div>
+                <div class="col-lg-6 col-md-11">
+                    <div class="rts-history-section">
+                        <h4 class="rts-section-title mb--40">Art Club</h4>
+                        <p>
+                            Art education at Pioneer is more than just drawing—it's a medium of exploration and
+                            problem-solving. Students are encouraged to think creatively and express ideas visually.
+                        </p>
+                        <p>The Annual Art Exhibition showcases their talent through a wide range of works, including:
+                        <ul>
+                            <li>Paintings</li>
+                            <li>Sculptures</li>
+                            <li>Posters</li>
+                            <li>Murals</li>
+                            <li>Photographs</li>
+                            <li>Pencil Sketches</li>
+                            <li>Mixed Media Creations</li>
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+
             </div>
-            <!-- mission -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="rts-timeline-section">
-                        <div class="rts-timeline-content">
-                            <div class="left-side">
-                                <div class="single-timeline-item">
-                                    <h5 class="timeline-title"> Academic Excellence & Values</h5>
-                                    <p>We combine values and modern education to nurture confident, future-ready global
-                                        citizens.
-                                    </p>
-                                    <img src="{{ asset('assets/images/about/mission-1.jpg') }}" alt="">
-                                </div>
-                                <div class="single-timeline-item">
-                                    <h5 class="timeline-title">Whole Child Development</h5>
-                                    <p>We promote balanced growth through academics, co-curricular activities, and
-                                        character-building experiences.
-                                    </p>
-                                    <img src="{{ asset('assets/images/about/mission-2.jpg') }}" alt="">
-                                </div>
-                            </div>
-                            <div class="separator">
-                            </div>
-                            <div class="right-side">
-                                <div class="single-timeline-item">
-                                    <h5 class="timeline-title">Holistic Growth</h5>
-                                    <p> We envision nurturing minds through creativity, compassion, leadership, and
-                                        student-centered learning environments.
-                                    </p>
-                                    <img src="{{ asset('assets/images/about/mission-3.jpg') }}" alt="">
-                                </div>
-                                <div class="single-timeline-item">
-                                    <h5 class="timeline-title">Future-Ready Leaders</h5>
-                                    <p>We aim to inspire innovative, ethical, and green leaders for tomorrow’s world.
-                                    </p>
-                                    <img src="{{ asset('assets/images/about/mission-4.jpg') }}" alt="">
-                                </div>
-                            </div>
-                        </div>
+        </div>
+    </div>
+    <!-- history end-->
+    <!-- history -->
+    <div class="rts-history" style="margin-bottom: 100px">
+        <div class="container">
+            <div class="row g-5 justify-content-md-center justify-content-start align-items-center">
+
+                <div class="col-lg-6 col-md-11">
+                    <div class="rts-history-section">
+                        <h4 class="rts-section-title mb--40">Eco Club</h4>
+                        <p>
+                            The Eco Club at Pioneer instills environmental awareness and responsibility in students. Through
+                            activities like plantation drives, recycling campaigns, and awareness programs, students learn
+                            to care for the planet and make sustainable choices.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-11">
+                    <div class="rts-history-image">
+                        <img src="{{ asset('assets/images/Co-curricular/eco.png') }}" alt="history">
                     </div>
                 </div>
             </div>
         </div>
-    </section> --}}
-    <!-- mission end-->
+    </div>
+    <!-- history end-->
 
-    @include($components . 'campus-tour', ['class' => 'rts-section-padding'])
-    @include($components . 'testimonial-v3', ['class' => 'rts-section-padding'])
+    {{-- @include($components . 'campus-tour', ['class' => 'rts-section-padding']) --}}
+    {{-- @include($components . 'testimonial-v3', ['class' => 'rts-section-padding']) --}}
     @include($components . 'newsletter', ['class' => 'rts-cta-background'])
     @include($footer . 'footer-v3', ['class' => 'v_2 pt--100 pb--100', 'cclass' => 'v_1'])
 @endsection
