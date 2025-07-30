f3c313ebc90a4760f34d6b03de1c9135434924ae
devrahul25 <rahulgoyal90000@gmail.com>
2025-07-30 15:45:03 +0530

    rahul chng

--- resources/views/Frontend/Partials/header/header-three.blade.php
+++ resources/views/Frontend/Partials/header/header-three.blade.php
@@ -10,7 +10,85 @@
                             </a>
                         </div> --}}
                     <div class="header__menu">
-                        @include($elements . 'nav__two')
+                        <div class="navigation">
+    <nav class="navigation__menu">
+        <ul>
+            <li class="navigation__menu--item has-child">
+                <a href="{{ route('index') }}" class="navigation__menu--item__link">Home</a>
+            </li>
+
+            <li class="navigation__menu--item has-child">
+                <a href="{{ route('about') }}" class="navigation__menu--item__link">About Us</a>
+                <ul class="submenu sub__style">
+                    <li><a href="{{ route('about') }}">Vision/Mission</a></li>
+                    <li><a href="{{ route('faculty-details') }}">Manager's Desk</a></li>
+                    <li><a href="{{ route('principal-desk') }}">Principal's Desk</a></li>
+                    <li><a href="{{ route('campus-life') }}">School Overview</a></li>
+                </ul>
+            </li>
+            <li class="navigation__menu--item has-child">
+                <a href="#" class="navigation__menu--item__link">Infrastructure</a>
+                <ul class="submenu sub__style">
+                    <li><a href="{{ route('academic') }}#campus-tour">Campus Tour</a></li>
+                    <li><a href="{{ route('academic') }}#library">Library</a></li>
+                    <li><a href="{{ route('academic') }}#computer-lab">Computer Lab</a></li>
+                    <li><a href="{{ route('academic') }}#physics-lab">Physics Lab</a></li>
+                    <li><a href="{{ route('academic') }}#chemistry-lab">Chemistry Lab</a>
+                    </li>
+                    <li><a href="{{ route('academic') }}#biology-lab">Biology Lab</a></li>
+                    <li><a href="{{ route('academic') }}#music-room">Music Room</a></li>
+                    <li><a href="{{ route('academic') }}#sports-room">Sports Room</a></li>
+                </ul>
+            </li>
+            <li class="navigation__menu--item has-child">
+                <a href="#" class="navigation__menu--item__link">Academics</a>
+                <ul class="submenu sub__style">
+                    <li><a href="{{ route('examinations') }}">Examinations</a></li>
+                    <li><a href="{{ route('academic-calender') }}">School Calendar</a></li>
+                    <li><a href="{{ route('faculty') }}">Faculty</a></li>
+                </ul>
+            </li>
+           
+                <div style="display: flex; align-item:center; justify-content:center;" class="header__logo">
+                            <a style="width: 100%;" href="{{ route('index') }}" class="header__logo--link">
+                                <img style="width: 100%;" src="{{ asset('assets/images/logo/logo.png') }}" alt="unipix">
+                            </a>
+                        </div>
+            
+            <li class="navigation__menu--item has-child">
+                <a href="#" class="navigation__menu--item__link">Activities</a>
+                <ul class="submenu sub__style">
+                    <li><a href="{{ route('athletics') }}">Sports</a></li>
+                    <li><a href="{{ route('co-curricular') }}">Co-Curricular</a></li>
+                </ul>
+            </li>
+            <li class="navigation__menu--item has-child">
+                <a href="#" class="navigation__menu--item__link">Admission</a>
+                <ul class="submenu sub__style">
+                    <li><a href="{{ route('admission') }}">Adm. Information</a></li>
+                    <li><a href="{{ route('tution-fee') }}">Fee Structure</a></li>
+                </ul>
+            </li>
+            <li class="navigation__menu--item has-child">
+                <a href="#" class="navigation__menu--item__link">Disclosure</a>
+                <ul class="submenu sub__style">
+                    <li><a href="{{ route('blog') }}">Mandatory Public Disclosure</a></li>
+                    <li><a href="{{ route('blog-grid') }}">SARAS-Mandatory Disclosure</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="navigation__menu--item has-child">
+                <a href="#" class="navigation__menu--item__link">Gallery</a>
+                <ul class="submenu sub__style">
+                    <li><a href="{{ route('image-gallery') }}">Photo Gallery</a></li>
+                    <li><a href="{{ route('video-gallery') }}">Video Gallery</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</div>
+
                     </div>
                     <div class="header__right">
                         <div class="header__right--item">
